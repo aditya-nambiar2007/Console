8f2b5c9fd3a5e4f6d189d37b294b56ab0e204594
aditya-nambiar2007 <99115432+aditya-nambiar2007@users.noreply.github.com>
2024-03-31 12:30:28 +0530

    Update code-1.py

--- code-1.py
+++ code-1.py
@@ -1,10 +1,10 @@
 from pynput.keyboard import Key,Controller
 from socketio  import Server,WSGIApp 
-import eventlet
+import eventlet,json,file
 from socket import gethostbyname as ip
 
 k=Controller()
-
+f=file.open("controls.json","r")
 def press(key):
 	k.press(key)
 def release(key):
@@ -13,16 +13,7 @@ def release(key):
 server=	Server()
 app=WSGIApp(server,static_files={'/':'console.html'})
 
-key_set= {
-		"Up"  :"w",
-		"Left":"a",
-		"Right":"d",
-		"Down":"s",		
-		"X"   :"a",
-		"A"   :Key.space,
-		"B"   :"d",
-		"Y"   :Key.space}
-
+key_set= 
 @server.on("message")
 def event(sid,data):
 	if data['e']=="ps":
